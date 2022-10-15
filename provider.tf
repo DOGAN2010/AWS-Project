a6dfd35534c7381cbd326e59d7b2beb29e50e907
DOGAN2010 <doganbilisim2010@gmail.com>
2022-10-15 12:18:40 +0300

    added file

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.33.0"
+    }
+    github = {
+      source = "integrations/github"
+      version = "5.5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "github" {
+  token = var.github_token #  PLEASE ENTER YOUR GITHUB TOKEN
+}
